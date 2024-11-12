468cd48370f0170fb6dca5d4ac1b6adef6952dfd
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2024-11-12 16:08:23 +0100

    ✨ feat(input): randomize input

--- src/prepare_input_from_lexique_tsv.py
+++ src/prepare_input_from_lexique_tsv.py
@@ -14,8 +14,14 @@ df = pd.read_csv(sys.argv[1], sep='\t')
 print("Extracting ortho and phon columns...")
 minimalDf = df[["ortho","phon","syll"]] # word, phonetics, phonetics with syllabus decomposition
 
+# Create a variant with randomized order
+# see: https://stackoverflow.com/questions/29576430/shuffle-dataframe-rows
+print("Creating a variant with randomized order...")
+randomizedMinimalDf = minimalDf.copy().sample(frac=1).reset_index(drop=True)
+
 # write to csv
 print("Writing to CSV file...")
 if not os.path.exists('out'):
     os.makedirs('out')
 minimalDf.to_csv('out/lexique_minimal.csv', index=False)
+randomizedMinimalDf.to_csv('out/lexique_minimal_randomized.csv', index=False)
