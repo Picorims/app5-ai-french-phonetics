892fac4d047639a6309b1e1668811279e7aa17a3
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2024-11-15 22:06:33 +0100

    ✨ feat(exec): process sentences in command line by splitting words in script

--- src/execute_model.py
+++ src/execute_model.py
@@ -11,12 +11,12 @@ import sys
 from pathlib import Path
 dirpath = Path(__file__).parent.absolute()
 
-if len(sys.argv) != 3:
+if len(sys.argv) < 3:
     print("Usage: python execute_model.py <model_name> <input_string>")
     sys.exit(1)
     
 model_name = sys.argv[1]
-input_text = sys.argv[2]
+input_text = " ".join(sys.argv[2:])
 
 print("Restoring model...")
 encoder_model, decoder_model, max_decoder_seq_length = mi.restore_model(model_name, dirpath)
@@ -24,8 +24,17 @@ restored_target_token_index = mi.restore_token_index(model_name, dirpath, "targe
 restored_input_token_index = mi.restore_token_index(model_name, dirpath, "input")
 
 print("Decoding sequence...")
-input_seq = mi.encode_text(input_text, max_decoder_seq_length, restored_input_token_index)
-decoded_sentence = mi.decode_sequence(input_seq, encoder_model, decoder_model, model_name, restored_target_token_index)
+for i,w in enumerate(sys.argv[2:]):
+    print("Decoding word: ", w)
+    input_seq = mi.encode_text(w, max_decoder_seq_length, restored_input_token_index)
+    decoded = mi.decode_sequence(input_seq, encoder_model, decoder_model, model_name, restored_target_token_index)
+    decoded = decoded.strip()
+    print("Decoded word: ", decoded)
+    if i == 0:
+        decoded_sentence = decoded
+    else:
+        decoded_sentence += " " + decoded
+    print("-------------------")
 
 print("================================================")
 print(f"Input sentence: {input_text}")
